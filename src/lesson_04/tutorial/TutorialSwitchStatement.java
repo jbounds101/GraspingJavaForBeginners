5ae7985931324c210b46f8fc4471b0f5aa12d158
Jackson <jackson.bounds@gmail.com>
2022-08-05 11:19:49 -0400

    lesson 04: switchStatement

--- src/lesson_04/tutorial/TutorialSwitchStatement.java
+++ src/lesson_04/tutorial/TutorialSwitchStatement.java
@@ -4,7 +4,8 @@ package lesson_04.tutorial;
 
 public class TutorialSwitchStatement {
     public static void main(String[] args) {
-        int position = 1;
+        int position = 4; // TODO Try changing this number to see what happens
+
         switch (position) { // A switch statement is essentially an if-else block, you specify a variable to run on
             // and use case statements to determine the effect
             case 1: // Notice the syntax
@@ -17,6 +18,18 @@ public class TutorialSwitchStatement {
             case 3:
                 System.out.println("You came in third-place!");
                 break;
+            default: // the default block is executed when the value isn't present in any case
+                System.out.println("You didn't do too well!");
+                break;
+        }
+
+
+        char myFavoriteCharacter = '$';
+        switch (myFavoriteCharacter) {
+            case '$' -> System.out.println("So much money!"); // Using the arrow syntax, you can exclude break
+            // statements, they cannot be combined in a single switch though!
+            case 'L' -> System.out.println("Loser!");
+            default -> System.out.println("I'm not sure");
         }
     }
 }
